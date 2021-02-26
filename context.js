8905f9a23057dae52624152978d1eaf2f0790986
Paolo Cuyno <paolo.cuyno@gmail.com>
2021-02-25 21:08:08 -0700

    lab

--- context.js
+++ context.js
@@ -18,6 +18,13 @@
 */
 
 //Code Here
+let user={
+  username:'',
+  email:'',
+  getUsername: function(){
+    return this.username
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -36,7 +43,7 @@ function Animal(name, species, food) {
     )
   }
 }
-
+let animal1= new Animal ('John','cat','food')
 //Code Here
 
 ////////// PROBLEM 3 //////////
@@ -56,7 +63,7 @@ let who = {
 }
 
 //Code Here
-
+let whoSaysHi=sayHi.bind(who)
 ////////// PROBLEM 4 //////////
 
 /*
@@ -68,7 +75,7 @@ function whatIsThis() {
 }
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 =whatIsThis()
 
 let product = {
   name: 'snake plant',
@@ -80,7 +87,7 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = product
 
 let vacation = {
   location: 'Hawaii',
@@ -91,7 +98,7 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation
 
 function Family(numParents, numKids, numPets) {
   this.numParents = numParents
@@ -106,4 +113,4 @@ function Family(numParents, numKids, numPets) {
 let family1 = new Family(2, 4, 1)
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-// let context4 = ???
+let context4 =family1
